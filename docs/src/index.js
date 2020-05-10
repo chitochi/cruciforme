194fe70a7717977dfe1726eec1d92d13bc30906e
Johyn Papin <johynpapin@koicactus.net>
2020-05-10 23:33:20 +0200

    Better website

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,5 +1,9 @@
 import hljs from 'highlight.js/lib/core'
 import xml from 'highlight.js/lib/languages/xml'
 
+hljs.configure({
+	tabReplace: '  '
+})
+
 hljs.registerLanguage('xml', xml)
 hljs.initHighlightingOnLoad()
