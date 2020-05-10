edc8a07e35d3bacae3b3ff3b97fa45ba7aa9e0c4
Chito <iamchito@protonmail.com>
2020-05-10 13:20:34 +0200

    Add cruciforme-subject

--- main.go
+++ main.go
@@ -14,55 +14,10 @@ func main() {
 	app.Use(limiter.New())
 	app.Use(logger.New())
 
-	app.Post("/action", func(c *fiber.Ctx) {
-		toMailAddress := c.FormValue("cruciforme-mail")
-		afterSuccess := c.FormValue("cruciforme-success")
-		afterError := c.FormValue("cruciforme-error")
+	app.Post("/", handleForm)
+	app.Post("/action", handleForm)
 
-		c.Locals("afterError", afterError)
-
-		multipartForm, err := c.MultipartForm()
-		if err != nil {
-			c.Next(err)
-			return
-		}
-
-		form := &Form{
-			ToMailAddress: toMailAddress,
-			AfterSuccess:  afterSuccess,
-			AfterError:    afterError,
-		}
-
-		for key, values := range multipartForm.Value {
-			if strings.HasPrefix(key, "cruciforme-") {
-				continue
-			}
-
-			form.Inputs = append(form.Inputs, &Input{
-				Name:  key,
-				Value: values[0],
-			})
-		}
-
-		for key, files := range multipartForm.File {
-			form.Files = append(form.Files, &File{
-				Name:       key,
-				FileHeader: files[0],
-			})
-		}
-
-		if err = form.sendByMail(); err != nil {
-			c.Next(err)
-		}
-
-		if form.AfterSuccess != "" {
-			c.Redirect(form.AfterSuccess, 303)
-		} else {
-			c.Send("Formulaire envoyé !")
-		}
-	})
-
-	app.Use("/action", func(c *fiber.Ctx) {
+	app.Use("/", func(c *fiber.Ctx) {
 		afterError, ok := c.Locals("afterError").(string)
 		if ok && afterError != "" {
 			c.Redirect(afterError, 303)
@@ -74,4 +29,54 @@ func main() {
 	})
 
 	app.Listen(8080)
+}
+
+func handleForm(c *fiber.Ctx) {
+	toMailAddress := c.FormValue("cruciforme-mail")
+	mailSubject := c.FormValue("cruciforme-subject")
+	afterSuccess := c.FormValue("cruciforme-success")
+	afterError := c.FormValue("cruciforme-error")
+
+	c.Locals("afterError", afterError)
+
+	multipartForm, err := c.MultipartForm()
+	if err != nil {
+		c.Next(err)
+		return
+	}
+
+	form := &Form{
+		ToMailAddress: toMailAddress,
+		MailSubject:   mailSubject,
+		AfterSuccess:  afterSuccess,
+		AfterError:    afterError,
+	}
+
+	for key, values := range multipartForm.Value {
+		if strings.HasPrefix(key, "cruciforme-") {
+			continue
+		}
+
+		form.Inputs = append(form.Inputs, &Input{
+			Name:  key,
+			Value: values[0],
+		})
+	}
+
+	for key, files := range multipartForm.File {
+		form.Files = append(form.Files, &File{
+			Name:       key,
+			FileHeader: files[0],
+		})
+	}
+
+	if err = form.sendByMail(); err != nil {
+		c.Next(err)
+	}
+
+	if form.AfterSuccess != "" {
+		c.Redirect(form.AfterSuccess, 303)
+	} else {
+		c.Send("Formulaire envoyé !")
+	}
 }
