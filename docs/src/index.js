0be2feaa13906dffaaa2a5df55a35da2a53c6b98
Chito <iamchito@protonmail.com>
2020-05-10 20:58:55 +0200

    Added a simple website

---
+++ docs/src/index.js
@@ -0,0 +1,5 @@
+import hljs from 'highlight.js/lib/core'
+import xml from 'highlight.js/lib/languages/xml'
+
+hljs.registerLanguage('xml', xml)
+hljs.initHighlightingOnLoad()
---
+++ docs/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: [
+		'./src/**/*.html'
+	],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
